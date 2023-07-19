5afb49e8a950f03d679acc5e9783198a326d5a67
Klaveeanick <137421533+Klaveeanick@users.noreply.github.com>
2023-07-19 15:14:50 +0300

    Update urls.py

--- app_advertisment/urls.py
+++ app_advertisment/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import index, top_sellers
 
 urlpatterns = [
-    path('', index, name='main_page'),
+    path('main-page', index, name='main_page'),
     path('top-sellers/', top_sellers, name='top_sellers')
 ]
